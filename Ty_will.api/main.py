f0281a42cfa82e33f949951aca373fe203f95a8d
Tyfabian-Williams-Software <123517574+Tyfabian-Williams-Software@users.noreply.github.com>
2023-02-02 19:19:20 -0600

    Add files via upload

---
+++ Ty_will.api/main.py
@@ -0,0 +1,10 @@
+# Tyfabian Williams
+# 2142655
+
+userNum = int(input())
+userNumSquared = userNum * userNum
+
+# Bug here; fix it when instructed
+# Output formatting issue here; fix it when instructed
+
+print(userNumSquared) 
